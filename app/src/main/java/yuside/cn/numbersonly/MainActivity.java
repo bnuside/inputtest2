8ce0317030ad5bc5f19b1fdd088afd758ed90a6b
bnuside <bnuside@gmail.com>
2018-09-04 18:32:06 +0800

    [feat] add autotest flavor

--- app/src/main/java/yuside/cn/numbersonly/MainActivity.java
+++ app/src/main/java/yuside/cn/numbersonly/MainActivity.java
@@ -1,16 +1,20 @@
 package yuside.cn.numbersonly;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,19 +25,29 @@ public class MainActivity extends AppCompatActivity {
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
 //        Button testButton = (Button)findViewById(R.id.testButton);
 //        testButton.setOnClickListener(mBtnListener);
 
-        setTitle(BuildConfig.FLAVOR);
+//        setTitle(BuildConfig.FLAVOR);
+        setFade();
+
+    }
+
+    public void setFade() {
+        LinearLayout layout = (LinearLayout)findViewById(R.id.lists);
+        final int sdk = Build.VERSION.SDK_INT;
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View v = layout.getChildAt(i);
+            if (v instanceof EditText) {
+                if (sdk < Build.VERSION_CODES.JELLY_BEAN) {
+                    v.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.color_stroke));
+                } else {
+                    v.setBackground(ContextCompat.getDrawable(this, R.drawable.color_stroke));
+                }
+
+                ((EditText) v).setHintTextColor(getResources().getColor(R.color.colorFadeText));
+            }
+        }
 
     }
 
@@ -58,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 
 //    private View.OnClickListener mBtnListener = new View.OnClickListener(){
 //        public void onClick(View v){
